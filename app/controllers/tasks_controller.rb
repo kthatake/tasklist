66a7d12686a015d95fc65766c485c6a51ab30a87
tako_2310 <kthatake@gmail.com>
2019-07-15 08:29:17 +0000

    for heroku2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
+  include SessionsHelper
+  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :edit]
+  
     def index
       @tasks = Task.all
     end
@@ -12,14 +17,14 @@ class TasksController < ApplicationController
     end
     
     def create
-      @task = Task.new(task_params)
-      
+      @task = current_user.tasks.build(task_params)
       if @task.save
         flash[:success] = "Taskが正常に登録されました"
-        redirect_to @task
+        redirect_to root_url
       else
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         flash.now[:danger] = "Taskが登録されませんでした"
-        render :new
+        render root_url
       end
     end
     
@@ -39,11 +44,10 @@ class TasksController < ApplicationController
     end
     
     def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success] = "タスクは正常に削除されました"
-      redirect_to tasks_url
+      redirect_to root_url
     end
   
   private
@@ -51,5 +55,12 @@ class TasksController < ApplicationController
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
