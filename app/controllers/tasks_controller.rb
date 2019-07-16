070347a67b34b2e6d71d9d929dd378d1d2389b8b
tako_2310 <kthatake@gmail.com>
2019-07-16 04:34:07 +0000

    for kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,10 @@ class TasksController < ApplicationController
   include SessionsHelper
   
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :edit]
+  before_action :correct_user, only: [:destroy, :edit, :update, :show]
   
     def index
-      @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc)
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: "tasks#index"
+  
+  get "signup", to: "users#new"
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get "signup", to: "users#new"
-  resources :tasks
   resources :users, only: [:new, :create]
+  resources :tasks
+  
 end
